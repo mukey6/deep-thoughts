7e4410a44997ad831c97dcbd4a95a333f56a7269
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-03-06 03:14:56 -0600

    login and logout functionality

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { setContext } from '@apollo/client/link/context';
 import Login from "./pages/Login";
 import NoMatch from "./pages/NoMatch";
 import SingleThought from "./pages/SingleThought";
@@ -20,9 +20,17 @@ import Home from "./pages/Home";
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
-
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
